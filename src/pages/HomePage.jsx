b4eb33c35dbbfe9631b74cba5ea69751854b951d
winnieHe7 <merphyhe@gmail.com>
2026-09-07 16:57:16 +0800

    align homepage statistics cards horizontally

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -118,6 +118,7 @@ export default function HomePage() {
           graduates={stats.graduates}
           active={stats.active}
           cities={stats.cities}
+          horizontal
           onViewFullStats={() => navigate('/stats')}
         />
 
